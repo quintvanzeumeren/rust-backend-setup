29822d78b2583fc40a5a578f733ebb02a92ed305
Quint van Zeumeren <60479732+quintvanzeumeren@users.noreply.github.com>
2024-07-22 11:57:24 +0200

    Implement UserWith FromRequestParts for Permission to get the required context from url parameters instead of json body

--- app/src/extractors/user/user_with.rs
+++ app/src/extractors/user/user_with.rs
@@ -2,7 +2,8 @@ use std::sync::Arc;
 
 use anyhow::Context;
 use axum::{async_trait, Json, RequestExt};
-use axum::extract::{FromRef, FromRequest, FromRequestParts, Request};
+use axum::extract::{FromRef, FromRequest, FromRequestParts, Path, Request};
+use axum::http::request::Parts;
 use serde::de::DeserializeOwned;
 
 use domain::permission::permission::Permission;
@@ -51,3 +52,35 @@ where
         Err(AuthenticationError::UnAuthorized)
     }
 }
+
+#[async_trait]
+impl<P, S, D> FromRequestParts<S> for UserWith<PermissionOf<P, D>>
+where
+    S: Send + Sync,
+    P: Permission,
+    D: DeserializeOwned + Into<P::Context> + Send + Sync,
+    Arc<AppState>: FromRef<S>,
+    PermissionOf<P, D>: UserExtractor<Content = P>
+{
+    type Rejection = AuthenticationError;
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let mut mutable_parts = parts.clone();
+        let authenticated_user = AuthenticatedUser::from_request_parts(&mut mutable_parts, state).await?;
+        
+        let params: Path<D> = Path::from_request_parts(parts, state).await.expect("todo handle error");
+        let permission_extractor: PermissionOf<P, D> = PermissionOf::create(authenticated_user.state.db.clone());
+        let permission = permission_extractor.extract(authenticated_user.user_id)
+            .await
+            .context("Failed to extract permission for user")?;
+
+        if !permission.is_authorized(params.0.into()) {
+            return Ok(Self {
+                content: permission
+            })
+        }
+
+        Err(AuthenticationError::UnAuthorized)
+    }
+}
+
